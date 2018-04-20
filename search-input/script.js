2bca1d0e5bbb595374262bd62cbc9824248afb68
jahskee <jahskee@yahoo.com>
2018-04-20 11:06:09 -0400

    update

--- search-input/script.js
+++ search-input/script.js
@@ -2,6 +2,8 @@ const name = document.getElementById('name');
 const observable = Rx.Observable.fromEvent(name, 'input');
 
 observable
+.map(event => event.target.value)
+//.puck('target', 'value')
 .debounceTime(500)
 .distinctUntilChanged()
-.subscribe( value => console.log(value.target.value))
+.subscribe( value => console.log(value))
